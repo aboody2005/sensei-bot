6b6be6d6c70243747118c86c1c2e238e1771ade1
unknown <abdulrahman.alshahwany@gmail.com>
2022-02-12 01:18:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const Discord = require('discord.js');
+// Create a new client instance
+const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const prefix =`?n`;
+
+bot.on('ready',()=>{
+    console.log("the bot is ready");
+})
+
+// bot.on('message',(msg)=>{
+// console.log(msg);
+// })
+
+// bot.on('message',(msg)=>{
+// if(msg.content=="hello"){
+//     msg.channel.send("hi")
+// }
+// })
+
+bot.on("message",(msga)=>{
+if(msga.content.startsWith(`${prefix} m`)){
+const msga_lenght =msga.content.length;
+const msga_splited=msga.content.slice(5,msga_lenght)
+var msga_num =eval(msga_splited);
+
+   msga.reply(msga_num);
+}
+
+})
+bot.on('message',(m1)=>{
+    if(m1.content.startsWith(`${prefix} help`)){
+       m1.reply("-  ?n m \n -  ?n help")
+    }
+})
+
+bot.login("OTQxNjgxNjQwODk4NDQ5NDI4.YgZfXg.8-H2VjLdvI5e4p1w9hMpKBXtV7E");
